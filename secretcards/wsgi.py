d5f8468d14942cdbf14e36b54b5ce88e8770c4b3
mlavin <markdlavin@gmail.com>
2016-03-27 08:51:21 -0400

    Update development requirements and fix new lint error.

--- secretcards/wsgi.py
+++ secretcards/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secretcards.settings")
-
-from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(get_wsgi_application())
